84781a54bf26befb31e65a159ec06ba25a4a8a3f
Melis <59087686+melis@users.noreply.github.com>
2022-02-08 09:57:39 +0600

    55

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import { useEffect } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
+import Categoryes from "./components/Categoryes/Categoryes";
+// import Header from "./components/Header/Header";
 
 function App() {
-  useEffect(() => {
-    //Humburger
-    const humburger = document.querySelector(".hamburger");
-    const header_categories = document.querySelector(".header__categories");
-
-    header_categories.addEventListener("click", function () {
-      humburger.classList.toggle("is-active");
-    });
-
-    // categories
-    const categories_btn = document.querySelectorAll(
-      ".categories__category--title"
-    );
-    const categories_list = document.querySelectorAll(
-      ".categories__category--list"
-    );
-
-    for (let i = 0; i < categories_btn.length; i++) {
-      let btn = categories_btn[i];
-      btn.addEventListener("click", function () {
-        for (let j = 0; j < categories_btn.length; j++) {
-          if (i !== j) {
-            categories_list[j].classList.remove("list-active");
-          }
-        }
-        categories_list[i].classList.toggle("list-active");
-      });
-    }
-
-    document.body.addEventListener("click", function (e) {
-      for (let i = 0; i < categories_btn.length; i++) {
-        if (
-          !categories_btn[i].contains(e.target) &&
-          !categories_list[i].contains(e.target)
-        ) {
-          categories_list[i].classList.remove("list-active");
-        }
-      }
-    });
-  }, []);
   return (
-    <div className="App">
-      <Header />
+    <div className="main">
+      <div className="container">
+        <Categoryes />
+      </div>
     </div>
   );
 }
---
+++ src/components/Categoryes/Categoryes.js
@@ -0,0 +1,283 @@
+import React from "react";
+
+function Categoryes(props) {
+  return (
+    <section className="categories">
+      {/* <!--            categories title--> */}
+      <div className="categories__title">
+        <h2 className="categories__title--text">Explore Categories</h2>
+      </div>
+      {/* <!--            categories title end--> */}
+      {/* <!--            categories block--> */}
+      <div className="categories__block">
+        {/* <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img src="images/marketplace/categories/beauty.png" alt="beauty" />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Beauty{" "}
+              <img
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list category__left">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end--> */}
+
+        {/* <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img src="images/marketplace/categories/baby.png" alt="baby" />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Baby{" "}
+              <img
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end-->
+
+              <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img
+              src="images/marketplace/categories/fashion.png"
+              alt="fashion"
+            />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Fashion{" "}
+              <img
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end-->
+
+              <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img
+              src="images/marketplace/categories/home-and-kitchen.png"
+              alt="home and kitchen"
+            />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Home & kitchen{" "}
+              <img
+                // style="transform: translate(5px, -8px);"
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end-->
+
+              <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img
+              src="images/marketplace/categories/health-and-haushold.png"
+              alt="health & household"
+            />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Health & household{" "}
+              <img
+                // style="transform: translate(5px, -8px);"
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end-->
+
+              <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img
+              src="images/marketplace/categories/electronics.png"
+              alt="electronics"
+            />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Electronics{" "}
+              <img
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end-->
+
+              <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img
+              src="images/marketplace/categories/luggage.png"
+              alt="luggage"
+            />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Luggage{" "}
+              <img
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end-->
+
+              <!--           category--> */}
+        <div className="categories__category">
+          <div className="categories__category--image">
+            <img src="images/marketplace/categories/sports.png" alt="sports" />
+          </div>
+          <div className="categories__category--body">
+            <span className="categories__category--title">
+              Sports{" "}
+              <img
+                src="./images/marketplace/categories/arrow.svg"
+                alt="arrow"
+              />
+            </span>
+            <div className="categories__category--list category__right">
+              <div className="categories__category--item">
+                Beauty & Personal Care
+              </div>
+              <div className="categories__category--item">Baby</div>
+              <div className="categories__category--item">Electronics</div>
+              <div className="categories__category--item">Fashion</div>
+              <div className="categories__category--item">
+                Health & Household
+              </div>
+              <div className="categories__category--item">Home & Kitchen</div>
+              <div className="categories__category--item">Luggage</div>
+              <div className="categories__category--item">Sports</div>
+            </div>
+          </div>
+        </div>
+        {/* <!--           category end  --> */}
+      </div>
+      {/* <!--            categories block end  --> */}
+    </section>
+  );
+}
+
+export default Categoryes;
