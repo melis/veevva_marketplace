ce89170d81cc225378fd642b288ac3770c91bd57
Melis <59087686+melis@users.noreply.github.com>
2022-02-12 09:55:44 +0300

    1

--- public/js/index.js
+++ public/js/index.js
@@ -1,47 +1,64 @@
 //Humburger
-const humburger = document.querySelector('.hamburger')
-const header_categories = document.querySelector('.header__categories--menu')
-const header_category = document.querySelector('.header__categories--category')
+const humburger = document.querySelector(".hamburger");
+const header_categories = document.querySelector(".header__categories--menu");
+const header_category = document.querySelector(".header__categories--category");
 
-header_categories.addEventListener('click', function() {
-  humburger.classList.toggle('is-active')
-  header_category.classList.toggle('header-category-active')
-})
+header_categories.addEventListener("click", function (e) {
+  e.stopPropagation();
+  humburger.classList.toggle("is-active");
+  header_category.classList.toggle("header-category-active");
+});
+document.body.addEventListener("click", function (e) {
+  humburger.classList.remove("is-active");
+  header_category.classList.remove("header-category-active");
+});
 
 // categories
-const categories_btn = document.querySelectorAll('.categories__category--title')
-const categories_list = document.querySelectorAll('.categories__category--list')
+// const categories_btn = document.querySelectorAll(
+//   ".categories__category--title"
+// );
+// const categories_list = document.querySelectorAll(
+//   ".categories__category--list"
+// );
 
-for(let i = 0; i < categories_btn.length; i++) {
-  let btn = categories_btn[i]
-  btn.addEventListener('click', function() {
-    for(let j = 0; j < categories_btn.length; j++) {
-      if(i != j) {
-        categories_list[j].classList.remove('list-active')
-      }
-    }
-    categories_list[i].classList.toggle('list-active')
-  })
-}
+// for (let i = 0; i < categories_btn.length; i++) {
+//   let btn = categories_btn[i];
+//   btn.addEventListener("click", function () {
+//     for (let j = 0; j < categories_btn.length; j++) {
+//       if (i != j) {
+//         categories_list[j].classList.remove("list-active");
+//       }
+//     }
+//     categories_list[i].classList.toggle("list-active");
+//   });
+// }
 
-document.body.addEventListener('click', function(e) {
-  for(let i = 0; i < categories_btn.length; i++) {
-    if(!categories_btn[i].contains(e.target) && !categories_list[i].contains(e.target)) {
-      categories_list[i].classList.remove('list-active')
-    }
-  }
-})
+// document.body.addEventListener("click", function (e) {
+//   console.log("dd");
+//   for (let i = 0; i < categories_btn.length; i++) {
+//     if (
+//       !categories_btn[i].contains(e.target) &&
+//       !categories_list[i].contains(e.target)
+//     ) {
+//       categories_list[i].classList.remove("list-active");
+//     }
+//   }
+// });
 
 // Filter
 
-const filter_header = document.querySelectorAll('#filter_header')
-const filter_submenu = document.querySelectorAll('#filter_submenu')
-const filter_arrow_bottom = document.querySelectorAll('.filter__title--arrow img')
+const filter_header = document.querySelectorAll("#filter_header");
+const filter_submenu = document.querySelectorAll("#filter_submenu");
+const filter_arrow_bottom = document.querySelectorAll(
+  ".filter__title--arrow img"
+);
 
-for(let i = 0, len = filter_header.length; i < len; i++) {
-  let head = filter_header[i]
-  head.addEventListener('click', function() {
-    filter_submenu[i].classList.toggle('active-submenu')
-    filter_submenu[i].classList.contains('active-submenu') ? filter_arrow_bottom[i].style.transform = 'rotate(180deg)' : filter_arrow_bottom[i].style.transform = 'rotate(0)'
-  })
+for (let i = 0, len = filter_header.length; i < len; i++) {
+  let head = filter_header[i];
+  head.addEventListener("click", function () {
+    filter_submenu[i].classList.toggle("active-submenu");
+    filter_submenu[i].classList.contains("active-submenu")
+      ? (filter_arrow_bottom[i].style.transform = "rotate(180deg)")
+      : (filter_arrow_bottom[i].style.transform = "rotate(0)");
+  });
 }
--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,22 +1,87 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Category({ category }) {
   const [active, setActive] = useState(false);
+
   return (
-    <div
+    <a
       className="categories__category"
-      // onMouseLeave={() => setActive(false)}
-      style={{ cursor: "pointer" }}
+      onMouseLeave={() => setActive(false)}
+      href={`./cartegory?c=${category.id}`}
     >
       <div className="categories__category--image" style={{ marginTop: "5px" }}>
         <img
           src={`./images/marketplace/categories/${category.category_path}`}
-          alt="beauty"
+          alt={category.category_name}
         />
       </div>
       <div className="categories__category--body">
         <span
           className="categories__category--title"
+          // style={{ marginBottom: "25px", marginLeft: "20px" }}
+          onClick={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            setActive((a) => !a);
+          }}
+        >
+          {category.category_name}
+          <img src="./images/marketplace/categories/arrow.svg" alt="arrow" />
+        </span>
+
+        <div
+          className={`categories__category--list ${active && "list-active"}`}
+        >
+          <a href="#" className="categories__category--item">
+            <div>Beauty & Personal Care</div>
+          </a>
+          <a href="#" className="categories__category--item">
+            <div>Baby</div>
+          </a>
+          <a href="#" className="categories__category--item">
+            <div>Electronics</div>
+          </a>
+          <a href="#" className="categories__category--item">
+            <div>Fashion</div>
+          </a>
+          <a href="#" className="categories__category--item">
+            <div>Health & Household</div>
+          </a>
+          <a href="#" className="categories__category--item">
+            <div>Home & Kitchen</div>
+          </a>
+          <a href="#" className="categories__category--item">
+            <div>Luggage</div>
+          </a>
+          <a href="#" className="categories__category--item">
+            <div>Sports</div>
+          </a>
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default Category;
+
+{
+  /* <div
+      classNameName="categories__category"
+      // onMouseLeave={() => setActive(false)}
+      style={{ cursor: "pointer" }}
+    >
+      <div
+        classNameName="categories__category--image"
+        style={{ marginTop: "5px" }}
+      >
+        <img
+          src={`./images/marketplace/categories/${category.category_path}`}
+          alt="beauty"
+        />
+      </div>
+      <div classNameName="categories__category--body">
+        <span
+          classNameName="categories__category--title"
           // style={{ marginBottom: "25px", marginLeft: "20px" }}
           onClick={() => setActive((a) => !a)}
         >
@@ -24,36 +89,35 @@ function Category({ category }) {
           <img src="./images/marketplace/categories/arrow.svg" alt="arrow" />
         </span>
         <div
-          className={`categories__category--list ${active && "list-active"}`}
+          classNameName={`categories__category--list ${
+            active && "list-active"
+          }`}
         >
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Beauty & Personal Care</a>
           </div>
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Baby</a>
           </div>
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Electronics</a>
           </div>
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Fashion</a>
           </div>
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Health & Household</a>
           </div>
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Home & Kitchen</a>
           </div>
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Luggage</a>
           </div>
-          <div className="categories__category--item">
+          <div classNameName="categories__category--item">
             <a href="#">Sports</a>
           </div>
         </div>
       </div>
-    </div>
-  );
+    </div> */
 }
-
-export default Category;
